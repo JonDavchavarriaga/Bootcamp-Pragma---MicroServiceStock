dcf71fc7c6f0ffe48d47e5ea1cfd0f022c37c712
Jonatan Chavarriaga <jonatanchavarriaga@gmail.com>
2024-08-21 22:47:39 -0500

    Added Aplication Test

---
+++ src/test/java/com/microservicestock/CategoryTest/AplicationTest/CategoryAplicationTest.java
@@ -0,0 +1,45 @@
+package com.microservicestock.CategoryTest.AplicationTest;
+
+import com.microservicestock.application.category.dto.CategoryDto;
+import com.microservicestock.application.category.handler.CategoryHandler;
+import com.microservicestock.application.category.mapper.CategoryDtoMapper;
+import com.microservicestock.domain.category.model.Category;
+import com.microservicestock.domain.category.port.service.ICategoryServicePort;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.*;
+
+class CategoryAplicationTest {
+
+    @Mock
+    private ICategoryServicePort categoryServicePort;
+    @Mock
+    private CategoryDtoMapper categoryDtoMapper;
+    @InjectMocks
+    private CategoryHandler categoryHandler;
+
+     @BeforeEach
+    public void setUp() {
+         MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    @DisplayName("Test save category Should map dto to domain and save")
+    void saveCategory_ShouldMapDtoToDomainAndCallService() {
+
+        CategoryDto categoryDto = new CategoryDto();
+        Category category = new Category("Toys", "Children's playthings");
+
+        when(categoryDtoMapper.toDomain(categoryDto)).thenReturn(category);
+
+        categoryHandler.saveCategory(categoryDto);
+
+        verify(categoryDtoMapper, times(1)).toDomain(categoryDto);
+        verify(categoryServicePort, times(1)).saveCategory(category);
+    }
+}
--- src/test/java/com/microservicestock/CategoryTest/CategoryModelTest/CategoryModelTest.java
+++ src/test/java/com/microservicestock/CategoryTest/CategoryModelTest/CategoryModelTest.java
@@ -2,6 +2,7 @@ package com.microservicestock.CategoryTest.CategoryModelTest;
 
 import com.microservicestock.domain.category.model.Category;
 import com.microservicestock.domain.category.model.categoryConstants.categoryConstant;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -11,6 +12,7 @@ class CategoryModelTest {
 
 
     @Test
+    @DisplayName("Create Category Model Test with correct name and description")
     void CreateCategoryModelTest_withCorrectNameAndDescription() {
         String categoryName = "Toys";
         String categoryDescription = "Children's playthings";
@@ -21,6 +23,7 @@ class CategoryModelTest {
     }
 
     @Test
+    @DisplayName("Create Category Model Test with Null Name")
     void CreateCategoryModelTest_withNullName() {
         String categoryDescription = "Children's playthings";
         IllegalArgumentException exception = assertThrows(
@@ -30,6 +33,7 @@ class CategoryModelTest {
         assertEquals(categoryConstant.CATEGORY_NAME_REQUIRED, exception.getMessage());
     }
     @Test
+    @DisplayName("Create Category Model Test with Empty Name")
     void CreateCategoryModelTest_withEmptyName() {
         String categoryDescription = "Children's playthings";
         IllegalArgumentException exception = assertThrows(
@@ -39,6 +43,7 @@ class CategoryModelTest {
         assertEquals(categoryConstant.CATEGORY_NAME_REQUIRED, exception.getMessage());
     }
     @Test
+    @DisplayName("Create Category Model Test with Null Description")
     void CreateCategoryModelTest_withNullDescription() {
         String categoryName = "Toys";
         IllegalArgumentException exception = assertThrows(
@@ -48,6 +53,7 @@ class CategoryModelTest {
         assertEquals(categoryConstant.CATEGORY_DESCRIPTION_REQUIRED, exception.getMessage());
     }
     @Test
+    @DisplayName("Create Category Model Test with Empty Description")
     void CreateCategoryModelTest_withEmptyDescription() {
         String categoryName = "Toys";
         IllegalArgumentException exception = assertThrows(
@@ -57,6 +63,7 @@ class CategoryModelTest {
         assertEquals(categoryConstant.CATEGORY_DESCRIPTION_REQUIRED, exception.getMessage());
     }
     @Test
+    @DisplayName("Create Category Model Test with Name Exceeding Max Length")
     void CreateCategoryModelTest_withNameExceedingMaxLength() {
         String categoryName = "AA".repeat(categoryConstant.NAME_MAX_LEGTH);
         String categoryDescription = "Children's playthings";
@@ -67,6 +74,7 @@ class CategoryModelTest {
         assertEquals(categoryConstant.INVALID_CATEGORY_NAME, exception.getMessage());
     }
     @Test
+    @DisplayName("Create Category Model Test with Description Exceeding Max Length")
     void CreateCategoryModelTest_withDescriptionExceedingMaxLength() {
         String categoryName = "Toys";
         String categoryDescription = "AA".repeat(categoryConstant.DESCRIPTION_MAX_LENGTH);
