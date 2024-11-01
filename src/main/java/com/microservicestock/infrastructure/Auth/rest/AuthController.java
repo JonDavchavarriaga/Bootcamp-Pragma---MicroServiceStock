cea7b500ef8a28e9fb67027bf2817be669f33ee5
Jonatan Chavarriaga <jonatanchavarriaga@gmail.com>
2024-11-01 14:47:58 -0500

    sonarlint corrections

--- src/main/java/com/microservicestock/infrastructure/Auth/rest/AuthController.java
+++ src/main/java/com/microservicestock/infrastructure/Auth/rest/AuthController.java
@@ -7,7 +7,6 @@ import com.microservicestock.application.Jwt.RegisterRequest;
 import jakarta.annotation.security.PermitAll;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
